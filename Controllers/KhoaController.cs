b07fbc546aefc454fac74d90be1ac38f016c2e28
hv <hv@PM308-GV>
2023-11-06 20:53:51 +0700

    Add project files.

---
+++ Controllers/KhoaController.cs
@@ -0,0 +1,69 @@
+﻿using DemoApi.Models.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KhoaController : ControllerBase
+    {
+        private readonly QuanLySinhVienContext _context;
+        public KhoaController(QuanLySinhVienContext context)
+        {
+            _context = context;
+        }
+
+
+        //api get all data
+        [HttpGet("danh-sach-khoa-1")] 
+        public IActionResult DanhSachKhoa1()
+        {
+            var items = _context.Khoas.ToList();
+            return Ok(items);
+        }
+
+        [HttpGet("danh-sach-khoa-2")]
+        public List<Khoa> DanhSachKhoa2()
+        {
+            var items = _context.Khoas.ToList();
+            return items;
+        }
+
+        //api get 1 data
+        [HttpGet("thong-tin-khoa/{id}")]
+        public IActionResult ItemKhoa(string id)
+        {
+            var item = _context.Khoas.FirstOrDefault(x => x.Id == id);
+            return Ok(item);
+        }
+
+        //api delete 1 data
+        [HttpDelete("xoa-khoa/{id}")]
+        public IActionResult XoaKhoa(string id)
+        {
+            var item = _context.Khoas.FirstOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                _context.Khoas.Remove(item);
+                _context.SaveChanges();
+            }
+            return Ok();
+        }
+
+        //api post data
+        [HttpPost("them-moi-khoa")]
+        public IActionResult TaoKhoa()
+        {
+            return Ok();
+        }
+
+        //api put data
+        [HttpPut("cap-nhat-khoa")]
+        public IActionResult CapNhat()
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+using DemoApi.Models.Entity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<QuanLySinhVienContext>(c => c.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.DefaultModelsExpandDepth(-1));
+}
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
